935f2b2fd74a0ebdf0c72d2b2d37295f2499ff37
BackolarsAnna <annabackolars@gmail.com>
2024-12-16 16:07:11 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -16,3 +16,18 @@ server
 server.listen(3000, () => {
   console.log("Server started on port 3000");
 });
+
+server.post('/users', (req, res) => {
+    const { firstName, lastName, username, color } = req.query;
+    const db = new sqlite3.Database('./gik339-labb2.db');
+    const sql = 'INSERT INTO USERS (firstName, lastName, username, color) VALUES (?, ?, ?, ?)';
+    const params = [firstName, lastName, username, color];
+    db.run(sql, params, (err) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.send({ id: this.lastID, firstName, lastName, username, color });
+      }
+    });
+    db.close();
+  });
