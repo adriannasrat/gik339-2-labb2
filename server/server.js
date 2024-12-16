7b4ecf7821dec761c6d250d9f671216d34b6ecfb
Adrian <h22alian@du.se>
2024-12-16 16:08:13 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const server = express();
 const sqlite3 = require('sqlite3').verbose();
 
-
 server
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -17,17 +16,31 @@ server.listen(3000, () => {
   console.log("Server started on port 3000");
 });
 
-server.post('/users', (req, res) => {
-    const { firstName, lastName, username, color } = req.query;
-    const db = new sqlite3.Database('./gik339-labb2.db');
-    const sql = 'INSERT INTO USERS (firstName, lastName, username, color) VALUES (?, ?, ?, ?)';
-    const params = [firstName, lastName, username, color];
-    db.run(sql, params, (err) => {
-      if (err) {
-        res.status(500).send(err);
-      } else {
-        res.send({ id: this.lastID, firstName, lastName, username, color });
-      }
-    });
-    db.close();
+server.get('/users', (req, res) => {
+  const db = new sqlite3.Database('./gik339-labb2.db');
+  const sql = 'SELECT * FROM USERS';
+  db.all(sql, (err, rows) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.send(rows);
+    }
   });
+
+  db.close();
+});
+
+server.post('/users', (req, res) => {
+  const { firstName, lastName, username, color } = req.query;
+  const db = new sqlite3.Database('./gik339-labb2.db');
+  const sql = 'INSERT INTO USERS (firstName, lastName, username, color) VALUES (?, ?, ?, ?)';
+  const params = [firstName, lastName, username, color];
+  db.run(sql, params, (err) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.send({ id: this.lastID, firstName, lastName, username, color });
+    }
+  });
+  db.close();
+});
